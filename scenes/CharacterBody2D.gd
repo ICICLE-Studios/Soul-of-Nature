0e95dafa705da05bc12d845230211011dce138c3
Ifranvel <91840643+ifranvel@users.noreply.github.com>
2023-09-16 21:19:31 +0200

    Gravedad PARCHEADO
    Ya no hay gravedad xd

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -10,8 +10,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
