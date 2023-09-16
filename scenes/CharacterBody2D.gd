5de684e59272d01e70533453791e063fb0da7df4
Ifranvel <91840643+ifranvel@users.noreply.github.com>
2023-09-16 21:32:24 +0200

    Movimiento vertical
    Ya esta disponible el movimiento vertical

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -19,10 +19,16 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var horizontalInput = Input.get_axis("ui_left", "ui_right")
+	var verticalInput = Input.get_axis("ui_up", "ui_down")
+	if horizontalInput:
+		velocity.x = horizontalInput * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if verticalInput:
+		velocity.y = verticalInput * SPEED 
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
