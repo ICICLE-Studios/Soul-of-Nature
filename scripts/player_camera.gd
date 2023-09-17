6e6d790a7f9ff1f601b7f8cf6816d6025ef7d1fd
Iván Moreno <ivanmoreno1312@gmail.com>
2023-09-17 12:01:08 +0200

    Player Fireball attack
    - Set default window size to a maximized 1920x1080
    - Added an "attack" input map action
    - Simplified PlayerCamera logic
    - Increased Enemy chase area size
    - Removed extra points from NavigationRegion2D
    - Added placeholder Player sprite

--- scripts/player_camera.gd
+++ scripts/player_camera.gd
@@ -3,23 +3,10 @@ extends Camera2D
 ## Handles the main camera's movement.
 
 
-const mouse_look_range = 100.0
+const MOUSE_LOOK_RANGE = 0.2
 
 @onready var player = %Player
 
 
 func _physics_process(delta):
-	var player_position = player.position
-	var mouse_position = get_viewport().get_mouse_position()
-	var window_size = get_viewport().size
-	
-	var normalized_mouse_position_x = (float(mouse_position.x) - float(window_size.x / 2)) / float(window_size.x)
-	var normalized_mouse_position_y = (float(mouse_position.y) - float(window_size.y / 2)) / float(window_size.y)
-	
-	var mouse_look = Vector2(
-			normalized_mouse_position_x * mouse_look_range,
-			normalized_mouse_position_y * mouse_look_range)
-	
-	var target_camera_position = player_position + mouse_look
-	
-	position = target_camera_position
+	position = player.position + get_local_mouse_position() * MOUSE_LOOK_RANGE
--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,6 +1,17 @@
 extends CharacterBody2D
 
+
 const SPEED = 150.0
+
+@onready var attack_group = %Attacks
+
+var player_fireball_scene = preload("res://scenes/attacks/player_fireball.tscn")
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		attack()
+
 
 func _physics_process(_delta):
 	var horizontalInput = Input.get_axis("ui_left", "ui_right")
@@ -18,3 +29,8 @@ func _physics_process(_delta):
 
 	move_and_slide()
 
+
+func attack():
+	var fireball_instance = player_fireball_scene.instantiate()
+	fireball_instance.position = position
+	attack_group.add_child(fireball_instance)
